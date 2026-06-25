09e7ca5bea0741881ec6085da4fde9ab769b4b6c
Joel <joelm4mani@gmail.com>
2026-06-25 15:07:02 -0300

    Estruturas de repetição

---
+++ src/br/com/devdojo/introducao/Aula06EstruturasDeRepeticao02.java
@@ -0,0 +1,16 @@
+package br.com.devdojo.introducao;
+
+public class Aula06EstruturasDeRepeticao02 {
+    public static void main(String[] args) {
+        // Imprima todos os números pares de 0 até 100000
+
+
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println("numero par " + i);
+            }
+        }
+
+
+    }
+}
